cd68ca51eb8a623f8f38b88bed35ccca8f592afa
kemi msabeni <kemimsabeni@gmail.com>
2022-06-30 09:03:26 +0300

    calculator code

---
+++ index.js
@@ -0,0 +1,24 @@
+//  basic arithmetic calculator without a frontend
+
+//  The calculator should be able to perform basic operations like Addition, Subtraction, Multiplication, & Division
+
+function add(a,b){
+    console.log(a + b)
+  }
+  
+  function subtract(a,b){
+    console.log(a - b)
+  }
+  
+  function multiply(a,b){
+    console.log(a * b)
+  }
+  
+  function divide(a,b){
+    console.log(a / b)
+  }
+  
+  add(1,2)
+  subtract(4,2)
+  multiply(3,6)
+  divide(9,3)
